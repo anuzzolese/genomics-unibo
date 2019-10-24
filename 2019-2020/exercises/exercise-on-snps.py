6771afa8c05370d3cf64095cfbbfa279dc73b9b3
Andrea Giovanni Nuzzolese <andrea.nuzzolese@gmail.com>
2019-10-24 19:09:19 +0200

    Update.

--- 2019-2020/exercises/exercise-on-snps.py
+++ 2019-2020/exercises/exercise-on-snps.py
@@ -10,91 +10,93 @@ class AlleleVariation(ABC):
     @abstractclassmethod
     def isTransversion(self):
         pass
-    
+        
+
 class SNP(AlleleVariation):
     
-    def __init__(self, refAllele, altAllele, chromosome, position, snpId):
-        self.__refAllele = refAllele
-        self.__altAllele = altAllele
-        self.__chromosome = chromosome
-        self.__position = position
-        self.__id = snpId
-        
+    def __init__(self, refBase, altBase, snpId, snpPosition, chromosomeName):
+        #the information in input should be store
+        # as private attribute of class's individuals
+        self.__refBase = refBase
+        self.__altBase = altBase
+        self.__snpId = snpId
+        self.__snpPosition = snpPosition
+        self.__chromosomeName = chromosomeName
     
+    # A transition is any combination of 
+    # A/G, G/A, C/T, or T/C
     def isTransition(self):
-        if(self.__refAllele != self.__altAllele):
-            if self.__refAllele == "A" or self.__refAllele == "G":
-                if self.__altAllele == "A" or self.__altAllele == "G":
-                    return True
-                
-            if self.__refAllele == "C" or self.__refAllele == "T":
-                if self.__altAllele == "C" or self.__altAllele == "T":
-                    return True
-                
-            return False
+        if self.__refBase != self.__altBase:
+            if self.__refBase == "A" and self.__altBase == "G":
+                return True
+            elif self.__refBase == "G" and self.__altBase == "A":
+                return True
+            elif self.__refBase == "C" and self.__altBase == "T":
+                return True
+            elif self.__refBase == "T" and self.__altBase == "C":
+                return True
+            else: 
+                return False
         else:
             return False
         
-    
+    # A transversion is any case that is not a transition
     def isTransversion(self):
         return not self.isTransition()
-    
+
+#Implement a Chromosome class that provides four methods: 
+# - count_transitions(), which returns the number of transition SNPs
+# - count_transversions(), which returns the number of transversion SNPs
+# - addSNP(), which add a SNP object into the array of SNPs associated to the current Chromosome
+# - getName, which returns the string representing the name of the Chromosome
 class Chromosome:
     
     def __init__(self, name):
         self.__name = name
         self.__snps = []
-        
-    def getName(self):
-        return self.__name
-        
-    def addSNP(self, snp):
-        if not isinstance(snp, SNP):
-            raise TypeError("snp must be of type SNP.")
-        else:
-            self.__snps.append(snp)
-            
-    def countTransitions(self):
+    
+    def count_transitions(self):
         transitions = 0
         for snp in self.__snps:
-            if snp.isTransition():
+            if snp.isTransition:
                 transitions = transitions + 1
-                
+        
         return transitions
     
-    def countTransversion(self):
-        transversions = 0
-        for snp in self.__snps:
-            if snp.isTransversion():
-                transversions = transversions + 1
-            
-        return transversions
-
-
-if __name__ == '__main__':
-    chromosomes = dict()
-    with open('trio.sample.vcf') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter='\t')
-        line_count = 0
-        for row in csv_reader:
-            chromosomeName = row[0]
-            snpPosition = row[1]
-            snpId = row[2]
-            refAllele = row[3]
-            altAllele = row[4]
-            
-            snp = SNP(refAllele, altAllele, chromosomeName, snpPosition, snpId)
-            
-            chromosome = chromosomes.get(chromosomeName, None)
-            
-            if chromosome is None:
-                chromosome = Chromosome(chromosomeName)
-                chromosomes[chromosomeName] = chromosome
-            
-            chromosome.addSNP(snp)
-            
-    for key in chromosomes.keys():
-        chromosome = chromosomes[key]
-        trans = chromosome.countTransitions()
-        transv = chromosome.countTransversion()
-        print(chromosome.getName() + " has " + str(trans) + " transitions and " + str(transv) + " transversions.")
+    def count_transversions(self):
+        pass
+    
+    def addSNP(self, snp):
+        self.__snps.append(snp)
+    
+    def getName(self):
+        return self.__name
+    
+        
+with open('trio.sample.vcf') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='\t')
+    line_count = 0
+    
+    chromosomeDict = dict()
+    for row in csv_reader:
+        chromosomeName = row[0]
+        snpPosition = row[1]
+        snpId = row[2]
+        refAllele = row[3]
+        altAllele = row[4]
+        
+        chromosome = chromosomeDict.get(chromosomeName, None)
+        if chromosome is None:
+            chromosome = Chromosome(chromosomeName)
+            chromosomeDict[chromosomeName] = chromosome
+        
+        snp = SNP(refAllele, altAllele, snpId, snpPosition, chromosomeName)
+        
+        chromosome.addSNP(snp)
+        
+    for key in chromosomeDict.keys():
+        chromosome = chromosomeDict[key]
+        
+        transitions = chromosome.count_transitions()
+        print("# of transitions for " + chromosome.getName() + " = " + str(transitions))
+        
